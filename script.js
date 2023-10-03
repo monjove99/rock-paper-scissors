12d5f0aad7325b8f333a608007908bcb17b56601
monjove99 <monjove99@gmail.com>
2023-10-03 09:45:30 +0800

    Added playRound function

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function getComputerChoice () {
-    
+    const randomChoice = Math.floor(Math.random() * 3);
+
+    switch (randomChoice) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2: 
+            return "Scissors";
+        default:
+            return "unknown";
+    }
+}
+
+function playRound (playerSelection, computerSelection) {
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+function game () {
+
 }
