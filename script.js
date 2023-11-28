f5ee52047ac55eb63c27495dd26851e0ab61a8ae
monjove99 <monjove99@gmail.com>
2023-11-29 06:52:47 +0800

    Added an event listener to the buttons and a function that displays winner

--- script.js
+++ script.js
@@ -34,32 +34,64 @@ function playRound (playerSelection, computerSelection) {
 
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    for (let round = 1; round <= 5; round++) {
-        const playerSelection = prompt("Round " + round + ": Rock, Paper, or Scissors?");
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
+//     for (let round = 1; round <= 5; round++) {
+//         const playerSelection = prompt("Round " + round + ": Rock, Paper, or Scissors?");
+//         const computerSelection = getComputerChoice();
+//         const result = playRound(playerSelection, computerSelection);
 
-        console.log(result);
+//         console.log(result);
 
-        if(result.includes("win")) {
-            playerScore++;
-        } else if (result.includes("lose")) {
-            computerScore++;
-        }
+//         if(result.includes("win")) {
+//             playerScore++;
+//         } else if (result.includes("lose")) {
+//             computerScore++;
+//         }
 
-    }
+//     }
 
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You win with a score of " + playerScore + "-" + computerScore + ".");
-    } else if (playerScore < computerScore) {
-        console.log("Game Over! You lose with a score of " + playerScore + "-" + computerScore + ".");
-    } else {
-        console.log("Its a tie with a score of " + playerScore + "-" + computerScore + ".")
+//     if (playerScore > computerScore) {
+//         console.log("Congratulations! You win with a score of " + playerScore + "-" + computerScore + ".");
+//     } else if (playerScore < computerScore) {
+//         console.log("Game Over! You lose with a score of " + playerScore + "-" + computerScore + ".");
+//     } else {
+//         console.log("Its a tie with a score of " + playerScore + "-" + computerScore + ".")
+//     }
+// }
+
+document.getElementById("rock").addEventListener("click", function() {
+    const computerChoice = getComputerChoice();
+    const resultMessage = playRound("rock", computerChoice);
+    updateResult(resultMessage);
+});
+
+document.getElementById("paper").addEventListener("click", function () {
+    const computerChoice = getComputerChoice();
+    const resultMessage = playRound("paper", computerChoice);
+    updateResult(resultMessage);
+});
+document.getElementById("scissors").addEventListener("scissors", function () {
+    const computerChoice = getComputerChoice();
+    const resultMessage = playRound("scissors", computerChoice);
+    updateResult(resultMessage);
+});
+
+function updateResult(message) {
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = message;
+
+    const scoreElement = document.createElement("p");
+    scoreElement.textContent = `Player: ${playerScore} | Computer: ${computerChoice}`;
+    resultElement.appendChild(scoreElement);
+
+    if (playerScore === 5 || computerScore === 5) {
+        const winner = playerScore === 5 ? "Player" : "Computer";
+        resultElement.textContent = `${winner} wins the game!`;
+        playerScore = 0;
+        computerScore = 0;
     }
 }
 
-game();
